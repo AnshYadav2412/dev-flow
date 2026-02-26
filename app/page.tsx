b2428467bf1976560116fbf3a235784e64f20e19
AnshYadav2412 <2412anshyadav@gmail.com>
2026-02-26 09:17:01 +0530

    Assets and Metadata added

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      <h1>Hello from Next.JS</h1>
+      <h1 className="font-inter">Hello from Next.JS</h1>
+      <h1>Hello Again</h1>
+      <h1 className="font-space-grotesk">Hellooooo Againn</h1>
     </div>
   );
 };
